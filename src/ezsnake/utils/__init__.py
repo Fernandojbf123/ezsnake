e26a2211a161f620d0794f714ec4675abfa7be51
bellodev <fernandojbf123@gmail.com>
2026-05-27 11:23:42 -0600

    FIX: bug_008 solved. Functions are now included

--- src/ezsnake/utils/__init__.py
+++ src/ezsnake/utils/__init__.py
@@ -5,6 +5,9 @@ from .functions import (
     grados_a_km_lon,
     distancia_entre_dos_puntos,
     calcular_tiempo_de_viaje,
+    timestamp_a_texto_espanol,
+    get_excel_variables_name,
+    get_excel_variable_values,
 )
 from .coords_converter import (
     convertir_cualquier_coordenada_a_grados_decimales,
@@ -18,6 +21,9 @@ __all__ = [
     "grados_a_km_lon",
     "distancia_entre_dos_puntos",
     "calcular_tiempo_de_viaje",
+    "timestamp_a_texto_espanol",
+    "get_excel_variables_name",
+    "get_excel_variable_values",
     "convertir_cualquier_coordenada_a_grados_decimales",
     "convertir_cualquier_coordenada_a_grados_y_minutos"
 ]
